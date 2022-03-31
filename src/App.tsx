1a98695d40c7f1032de444463a20edfdee30f42f
skov82 <skov82@ya.ru>
2022-04-01 00:01:05 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Rating from "./components/Rating";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <Rating value={4} />
-      </header>
+     Hello, samurai! Let's go!
     </div>
   );
 }
