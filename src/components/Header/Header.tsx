cf14bf928de50bc8b5edba6739d53e00112b49de
skov82 <skov82@ya.ru>
2022-04-06 23:45:52 +0300

    added NavLink, some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from 'react-router-dom';
 
 export function Header() {
     return (
         <header className={classes.header}>
             <img className={classes.app_logo} src="https://www.pinclipart.com/picdir/big/373-3735542_phoenix-banfield-illustration-clipart.png" alt="logo"/>
 
-            <nav className="text-center">
-                <a href="/">Главная</a> | <a href="/news">Новости</a> | <a href="/about">Контакты</a>
+            <nav className="text-center space-x-20">
+                <NavLink to={'/'} className="">Главная</NavLink>
+                <NavLink to={'/news'} className=""> Новости</NavLink>
+                <NavLink to={'/about'} className=""> Контакты</NavLink>
             </nav>
         </header>
     );
--- src/components/Main/Navbar.tsx
+++ src/components/Main/Navbar.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 export function Navbar() {
     return (
         <aside className={classes.sidebar}>
-            <nav>
-                <ul>
-                    <li><a href="/profile">Профиль</a></li>
-                    <li><a href="/dialogs">Сообщения</a></li>
-                    <li><a href="/friends">Друзья</a></li>
-                    <li><a href="/groups">Группы</a></li>
-                    <li><a href="/settings">Настройки</a></li>
-                </ul>
+            <nav className="grid grid-cols-1 gap-4 pl-7 pt-10">
+                <NavLink to={'/profile'} className="hover:scale-110 hover:skew-y-3">Профиль</NavLink>
+                <NavLink to={'/dialogs'} className="hover:scale-110 hover:skew-y-3">Сообщения</NavLink>
+                <NavLink to={'/friends'} className="hover:scale-110 hover:skew-y-3">Друзья</NavLink>
+                <NavLink to={'/groups'} className="hover:scale-110 hover:skew-y-3">Группы</NavLink>
+                <NavLink to={'/settings'} className="hover:scale-110 hover:skew-y-3">Настройки</NavLink>
             </nav>
         </aside>
     );
