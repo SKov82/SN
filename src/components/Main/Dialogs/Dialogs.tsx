64fcdc9da0b594c2231ae4cec2c6f79a049f907a
skov82 <skov82@ya.ru>
2022-04-07 21:15:49 +0300

    added subcomponents, props types into Dialogs component

--- src/components/Main/Dialogs/Dialogs.tsx
+++ src/components/Main/Dialogs/Dialogs.tsx
@@ -1,30 +1,53 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
-export function Dialogs(props:any) {
+type DialogType = {
+    id: number
+    name: string
+}
+
+type MessageType = {
+    id: number
+    text: string
+}
+
+function Dialog(props: DialogType) {
+    return (
+        <NavLink to={`/dialogs/${props.id}`}
+                 className="font-semibold hover:scale-105 hover:skew-y-2"
+                 activeClassName={classes.active}>
+            {props.name}
+        </NavLink>
+    )
+}
+
+function Message(props: MessageType) {
+    return (
+        <div className={classes.message}>
+            {props.text}
+        </div>
+    )
+}
+
+export function Dialogs() {
     return (
         <div className={classes.content}>
             <div className={classes.chats}>
                 <div>Чаты</div>
 
-                <ul>
-                    <li>Ирина</li>
-                    <li>Юля</li>
-                    <li>Вова</li>
-                    <li>Миша</li>
-                </ul>
+                <nav className="grid grid-cols-1 gap-4 pl-7 pt-7">
+                    <Dialog id={1} name={'Ирина'}/>
+                    <Dialog id={2} name={'Юля'}/>
+                    <Dialog id={3} name={'Вова'}/>
+                    <Dialog id={4} name={'Миша'}/>
+                </nav>
             </div>
 
             <div className={classes.messages}>
-                <div className={classes.message}>
-                    Привет
-                </div>
-                <div className={classes.message}>
-                    Ты дома?
-                </div>
-                <div className={classes.message}>
-                    Привет. Да, только пришел
-                </div>
+                <Message id={1} text={'Привет'}/>
+                <Message id={2} text={'Ты дома?'}/>
+                <Message id={3} text={'Привет. Да, только пришел'}/>
             </div>
         </div>
     )
