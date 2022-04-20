2785208e082dced1df0ec2d650f71c04ba461b66
skov82 <skov82@ya.ru>
2022-04-21 01:21:12 +0300

    added text area for new messages

--- src/components/Main/Dialogs/Messages.tsx
+++ src/components/Main/Dialogs/Messages.tsx
@@ -17,6 +17,16 @@ function Message({text}: MessageType) {
 export function Messages({messages}: any) {
     return <>
         {messages.map((el: MessageType) => <Message key={el.id} id={el.id} text={el.text}/>)}
+
+        <div className={css.new_message}>
+            <textarea name="new_message" id="new_message" rows={3}></textarea>
+
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    onClick={ () => {} }
+            >
+                Отправить
+            </button>
+        </div>
     </>
 }
 
