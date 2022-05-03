a00f667bb225b064c149ab54057b1c4f5b438633
skov82 <skov82@ya.ru>
2022-05-04 02:40:09 +0300

    add onChange, onClick and onKeyDown Handlers for new messages

--- src/components/Main/Dialogs/Messages.tsx
+++ src/components/Main/Dialogs/Messages.tsx
@@ -1,6 +1,9 @@
 import css from './Messages.module.css';
 import React from 'react';
-import {DispatchActionType, MessageType} from '../../../state';
+import {
+    addMessageActionCreator, DispatchActionType,
+    MessageType, updateNewMessageActionCreator,
+} from '../../../state';
 
 function Message({text}: MessageType) {
     return (
@@ -17,18 +20,31 @@ type MessagesPropsType = {
 }
 
 export function Messages({messages, newMessageText, dispatch}: MessagesPropsType) {
+    let newMessageElement = React.createRef<HTMLTextAreaElement>()
+    const addMessageHandler = () => {
+        if (newMessageElement.current && newMessageElement.current.value.trim()) {
+            dispatch( addMessageActionCreator() )
+        }
+    }
+    const onChangeMessageHandler = () => {
+        if (newMessageElement.current) dispatch( updateNewMessageActionCreator(newMessageElement.current.value) )
+    }
+
     return <>
         {messages.map((el: MessageType) => <Message key={el.id} id={el.id} text={el.text}/>)}
 
         <div className={css.new_message}>
             <textarea value={newMessageText}
+                      ref={newMessageElement}
+                      onChange={onChangeMessageHandler}
+                      onKeyDown={ (e) => {if (e.key === 'Enter') addMessageHandler()} }
                       name="new_message"
                       id="new_message"
                       rows={3}
             />
 
             <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                    onClick={ () => {} }
+                    onClick={ addMessageHandler }
             >
                 Отправить
             </button>
