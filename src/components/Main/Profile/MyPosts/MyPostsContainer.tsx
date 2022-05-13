e6e5ea7cc7ca7105bcc66b6ccae928ad13a0788b
skov82 <skov82@ya.ru>
2022-05-13 22:51:19 +0300

    add types

--- src/components/Main/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Main/Profile/MyPosts/MyPostsContainer.tsx
@@ -4,15 +4,21 @@ import {
 } from '../../../../redux/profile-reducer';
 import {connect} from 'react-redux';
 import {AppStateType} from '../../../../redux/redux-store';
+import {Dispatch} from 'redux';
 
 type mapStateToPropsType = {
     profileData: ProfileDataType
 }
 
+type mapDispatchToPropsType = {
+    updatePost: (text: string) => void
+    addPost: () => void
+}
+
 let mapStateToProps = (state: AppStateType): mapStateToPropsType => {
     return { profileData: state.profileData }
 }
-let mapDispatchToProps = (dispatch: any) => {
+let mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
     return {
         updatePost: (text: string) => {
             dispatch( updateNewPostActionCreator(text) )
