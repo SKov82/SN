22fffe7ddda96e32dc15bd31e82bbe2971f38d33
skov82 <skov82@ya.ru>
2022-05-31 21:19:13 +0300

    add types and AC for authReducer

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,19 +1,26 @@
+type StateType = {
+    id: null | number
+    login: null | string
+    email: null | string
+}
 export type AuthDataType = {
-    id: number | null
-    login: string
-    email: string
+    data: StateType
     isLoading: boolean
 }
 
 let initialState: AuthDataType = {
-    id: null,
-    login: '',
-    email: '',
+    data: {
+        id: null,
+        login: null,
+        email: null,
+    },
     isLoading: false
 }
 
 export const authReducer = (state: AuthDataType = initialState, action: ActionType): AuthDataType => {
     switch (action.type) {
+        case 'SET-AUTH-DATA':
+            return {...state, data: action.data}
         case 'CHANGE-LOADING-STATUS':
             return {...state, isLoading: !state.isLoading}
         default:
@@ -21,8 +28,10 @@ export const authReducer = (state: AuthDataType = initialState, action: ActionTy
     }
 }
 
-export type ActionType = ChangeLoadingStatusType
+export type ActionType = ChangeLoadingStatusType | SetAuthDataType
 
+type SetAuthDataType = ReturnType<typeof setAuthData>
 type ChangeLoadingStatusType = ReturnType<typeof changeLoadingStatus>
 
+export const setAuthData = (data: StateType) => ({ type: 'SET-AUTH-DATA', data } as const)
 export const changeLoadingStatus = () => ({ type: 'CHANGE-LOADING-STATUS' } as const)
