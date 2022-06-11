8c1fced6c62d23dce6c6e120e8624d70489568c4
skov82 <skov82@ya.ru>
2022-06-11 16:16:08 +0300

    refactor

--- src/components/Main/Profile/ProfileContainer.tsx
+++ src/components/Main/Profile/ProfileContainer.tsx
@@ -3,10 +3,11 @@ import {Profile, ProfileType} from './Profile';
 import {connect} from 'react-redux';
 import {getUserProfile} from '../../../redux/profile-reducer';
 import {AppStateType} from '../../../redux/redux-store';
-import {withRouter} from 'react-router-dom';
+import {Redirect, withRouter} from 'react-router-dom';
 
 type ProfileContainerType = {
     profileData: ProfileType
+    isAuth: boolean
     getUserProfile: (profile: ProfileType) => void
 }
 
@@ -18,6 +19,7 @@ class ProfileC extends React.Component<ProfileContainerType> {
     }
 
     render () {
+        if (!this.props.isAuth) return <Redirect to={'/login'} />
         return (
             <Profile {...this.props.profileData} />
         )
@@ -26,9 +28,12 @@ class ProfileC extends React.Component<ProfileContainerType> {
 
 type MapStateToPropsType = {
     profileData: null | ProfileType
+    isAuth: boolean
 }
 
-let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({ profileData: state.profileData.profile })
+let mapStateToProps = (state: AppStateType): MapStateToPropsType => {
+    return { profileData: state.profileData.profile, isAuth: state.auth.isAuth }
+}
 
 // @ts-ignore
 let UrlDataContainer = withRouter(ProfileC)
