dd3a262afd5f53bb611df952fb06c63673eccdfe
skov82 <skov82@ya.ru>
2022-06-14 22:50:04 +0300

    add update/change UserStatus TC, AC, Reducer case

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -39,21 +39,25 @@ export const profileReducer = (state: ProfileDataType = initialState, action: Ac
             return {...state, profile: action.profile}
         case 'SET_USER_STATUS':
             return {...state, status: action.status}
+        case 'CHANGE_USER_STATUS':
+            return {...state, status: action.newStatus}
         default:
             return state
     }
 }
 
-type ActionType = AddNewPostType | UpdateNewPostType | SetUserProfileType | SetUserStatusType
+type ActionType = AddNewPostType | UpdateNewPostType | SetUserProfileType | SetUserStatusType | ChangeUserStatusType
 type AddNewPostType = ReturnType<typeof addNewPost>
 type UpdateNewPostType = ReturnType<typeof updateNewPost>
 type SetUserProfileType = ReturnType<typeof setUserProfile>
 type SetUserStatusType = ReturnType<typeof setUserStatus>
+type ChangeUserStatusType = ReturnType<typeof changeUserStatus>
 
 export const addNewPost = () => ({ type: 'ADD_POST' } as const)
 export const updateNewPost = (post: string) => ({ type: 'UPDATE_NEW_POST_TEXT', post } as const)
 const setUserProfile = (profile: ProfileType) => ({ type: 'SET_USER_PROFILE', profile } as const)
 const setUserStatus = (status: string | null) => ({ type: 'SET_USER_STATUS', status } as const)
+const changeUserStatus = (newStatus: string) => ({ type: 'CHANGE_USER_STATUS', newStatus } as const)
 
 export const getUserProfile = (userID: number) => {
     return (dispatch: Dispatch) => {
@@ -66,6 +70,13 @@ export const getUserStatus = (userID: number) => {
     return (dispatch: Dispatch) => {
         profileAPI.getUserStatus(userID).then(data => {
             dispatch(setUserStatus(data))
+        })
+    }
+}
+export const updateUserStatus = (newStatus: string) => {
+    return (dispatch: Dispatch) => {
+        profileAPI.updateUserStatus(newStatus).then(data => {
+            dispatch(changeUserStatus(data))
         })
     }
 }
