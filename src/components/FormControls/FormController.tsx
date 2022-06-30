cebcdc26bbec30afcc16c699a8b18bf790371fab
skov82 <skov82@ya.ru>
2022-06-30 13:22:24 +0300

    add styles for textarea

--- src/components/FormControls/FormController.tsx
+++ src/components/FormControls/FormController.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import css from './FormController.module.css'
 
 export const TextArea = ({input, meta, ...props}: any) => {
-    return <textarea {...input} {...props} />
+    return <>
+        <textarea {...input}
+                  {...props}
+                  className={css.formControl + ' ' + (meta.active && meta.error ? css.error : '')}
+        />
+        {meta.active && meta.error && <span className={css.error}> {meta.error} </span>}
+    </>
 }
