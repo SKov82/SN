56f99f6bb2e370f133e3ba7bb4e35f42c97abd0c
skov82 <skov82@ya.ru>
2022-07-13 18:06:45 +0300

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import {Footer} from './components/Footer';
 import {HeaderContainer} from './components/Header/HeaderContainer';
 import {connect} from 'react-redux';
 import {getInit} from './redux/app-reducer';
+import {AppStateType} from './redux/redux-store';
+import css from './components/Main/Users/Users.module.css';
+import preloader from './assets/img/loading.gif';
 
 type AppType = {
+    isInit: boolean
     getInit: () => void
 }
 class App extends React.Component<AppType> {
@@ -15,14 +19,22 @@ class App extends React.Component<AppType> {
     }
 
     render() {
-        return <div className="App">
-            < //@ts-ignore
-                HeaderContainer
-            />
-            <Main/>
-            <Footer/>
-        </div>
+        if (this.props.isInit) {
+            return <div className="App">
+                < //@ts-ignore
+                    HeaderContainer
+                />
+                <Main/>
+                <Footer/>
+            </div>
+        } else {
+            return <img className={css.loading} src={preloader} alt="loading..."/>
+        }
     }
 }
+
+let mapStateToProps = (state: AppStateType): {isInit: boolean} => {
+    return {isInit: state.app.isInit}
+}
 // @ts-ignore
-export default connect(null, {getInit})(App)
+export default connect(mapStateToProps, {getInit})(App)
