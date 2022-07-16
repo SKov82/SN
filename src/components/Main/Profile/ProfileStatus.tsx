598f68a743f6590a7decb05e16bdcda39b6b1eca
skov82 <skov82@ya.ru>
2022-07-16 20:36:24 +0300

    refactor with hooks

--- src/components/Main/Profile/ProfileStatus.tsx
+++ src/components/Main/Profile/ProfileStatus.tsx
@@ -1,21 +1,21 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppStateType} from '../../../redux/redux-store';
 import {updateUserStatus} from '../../../redux/profile-reducer';
 
 export const ProfileStatus = (props: {userID: number, status: string | null}) => {
+    const dispatch = useDispatch()
+    // const status = useSelector<AppStateType, string | null>(state => state.profileData.status)
+    const [status, setStatus] = useState<string | null>(props.status)
     const [editMode, setEditMode] = useState<boolean>(false)
     const editModeHandler = () => setEditMode(!editMode)
 
-    const dispatch = useDispatch()
-    const [status, setStatus] = useState<string | null>(
-        useSelector<AppStateType, string | null>(state => state.profileData.status)
-    )
+    useEffect(() => setStatus(props.status), [props.status])
 
     return <div onDoubleClick={editModeHandler}>
         {`Статус: `}
         { editMode
-            ? <span onBlur={editModeHandler} >
+            ? <span onBlur={editModeHandler}>
                 <input value={status || props.status || ''}
                        onChange={ (e: ChangeEvent<HTMLInputElement>) => setStatus(e.currentTarget.value) }
                        onKeyPress={ (e: KeyboardEvent<HTMLInputElement>) => {
