a6b843ede0584e6f66d1595d34062896b1c4eff1
skov82 <skov82@ya.ru>
2022-08-04 14:27:44 +0300

    fix

--- src/components/Main/Profile/ProfileStatus.test.js
+++ src/components/Main/Profile/ProfileStatus.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
+import TestRenderer, {act} from 'react-test-renderer';
 import {store} from '../../../redux/redux-store';
 import {Provider} from 'react-redux';
 import {ProfileStatus} from './ProfileStatus';
 
-
-it('render ProfileStatus component', () => {
+test('render ProfileStatus component', () => {
     const testRenderer = TestRenderer.create(
         <Provider store={store}>
             <ProfileStatus userID={23880} status={'OK'} />
@@ -14,7 +13,14 @@ it('render ProfileStatus component', () => {
     const testInstance = testRenderer.root
 
     expect(testRenderer.toJSON().children[1].children[0]).toBe('OK')
+
     expect(testInstance.findByType(ProfileStatus).props.userID).toBe(23880)
     expect(testInstance.findByType(ProfileStatus).props.status).toBe('OK')
+
     expect(testInstance.findByType('span').children).toStrictEqual(['OK'])
+
+    act(() => {
+        testInstance.findByType('div').props.onDoubleClick()
+    })
+    expect(testInstance.findByType('input').props.value).toBe('OK')
 })
