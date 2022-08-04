3609ea6672005a43195829dc8c89833c46a3b4e3
skov82 <skov82@ya.ru>
2022-08-04 11:47:30 +0300

    add test App component

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './redux/redux-store';
+import {BrowserRouter} from 'react-router-dom';
+
+it('render App component', () => {
+    const div = document.createElement('div')
+
+    ReactDOM.render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>,
+    div)
+
+    ReactDOM.unmountComponentAtNode(div)
+})
